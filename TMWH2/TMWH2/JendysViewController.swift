c72b494901890b8b63063de85ebbb9809f360682
Margaret Kleiman <kleimanm17@hanover.edu>
2017-03-14 12:48:51 -0400

    viewControllers for each store

---
+++ TMWH2/TMWH2/JendysViewController.swift
@@ -0,0 +1,9 @@
+//
+//  JendysViewController.swift
+//  TMWH2
+//
+//  Created by Margaret Kleiman on 3/14/17.
+//  Copyright © 2017 Margaret Kleiman. All rights reserved.
+//
+
+import Foundation
---
+++ TMWH2/TMWH2/RedPepperoniViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RedPepperoniViewController.swift
+//  TMWH2
+//
+//  Created by Margaret Kleiman on 3/14/17.
+//  Copyright © 2017 Margaret Kleiman. All rights reserved.
+//
+
+import Foundation
